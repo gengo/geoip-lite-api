ec25a071cbc54ae6f3da6c396d9731b627f448a9
Mikhail Nathaniel Abordo <emyawasap@gmail.com>
2019-06-18 15:42:26 +0800

    Update Return Values

--- api.py
+++ api.py
@@ -11,7 +11,28 @@ class GeoIP:
     @hug.object.get(urls='/geoip/{ipv4}')
     def get(self, ipv4: str):
         try:
-            return self.reader.get(ipv4)
+            info = self.reader.get(ipv4)
+            # return info['subdivisions'][0]['']
+
+            result = {
+                "ip" : ipv4,
+                "continent_code" : info['continent']['code'],
+                "continent_name" : info['continent']['names']['en'],
+                "country_code" : info['country']['iso_code'],
+                "country_name" : info['country']['names']['en'],
+                "region_code" : info['subdivisions'][0]['iso_code'],
+                "region_name" : info['subdivisions'][0]['names']['en'],
+                "city" : info['city']['names']['en'],
+                "zip" : info['postal']['code'],
+                "latitude" : info['location']['latitude'],
+                "longitude" : info['location']['longitude'],
+                "location" : {
+                    "geonameid" : info['city']['geoname_id'],
+                    "time_zone" : info['location']['time_zone']
+                }
+            }
+
+            return result
         except ValueError:
             raise falcon.HTTPBadRequest('message', 'Invalid IPv4 address')
 

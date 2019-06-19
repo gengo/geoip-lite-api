42f8c6afbaf8e0a487e72835a24a9534ab05420f
Mikhail Nathaniel Abordo <emyawasap@gmail.com>
2019-06-19 12:59:24 +0800

    Update Return Values (#1)
    * Update Return Values
    
    * Update if Object Key is Missing
    
    * Update try clause and double quoted strings

--- api.py
+++ api.py
@@ -11,10 +11,53 @@ class GeoIP:
     @hug.object.get(urls='/geoip/{ipv4}')
     def get(self, ipv4: str):
         try:
-            return self.reader.get(ipv4)
+            info = self.reader.get(ipv4)
         except ValueError:
             raise falcon.HTTPBadRequest('message', 'Invalid IPv4 address')
 
+        result = {
+            'ip' : ipv4,
+            'continent_code' : '',
+            'continent_name' : '',
+            'country_code' : '',
+            'country_name' : '',
+            'region_code' : '',
+            'region_name' : '',
+            'city' : '',
+            'zip' : '',
+            'latitude' : '',
+            'longitude' : '',
+            'location' : {
+                'geonameid' : '',
+                'time_zone' : ''
+            }
+        }
+
+        if 'subdivisions' in info:
+            result['region_code'] = info['subdivisions'][0]['iso_code']
+            result['region_name'] = info['subdivisions'][0]['names']['en']
+
+        if 'city' in info:
+            result['city'] = info['city']['names']['en']
+            result['location']['geonameid'] = info['city']['geoname_id']
+
+        if 'postal' in info:
+            result['zip'] = info['postal']['code']
+
+        if 'continent' in info:
+            result['continent_code'] = info['continent']['code']
+            result['continent_name'] = info['continent']['names']['en']
+
+        if 'country' in info:
+            result['country_code'] = info['country']['iso_code']
+            result['country_name'] = info['country']['names']['en']
+
+        if 'location' in info:
+            result['latitude'] = info['location']['latitude']
+            result['longitude'] = info['location']['longitude']
+            result['location']['time_zone'] = info['location']['time_zone']
+
+        return result
 
 logging.info('Reading maxmind DB from {}'.format('path'))
 start = time.process_time()
